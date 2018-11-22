5a9a556042eef8dd491a65c55209bc4395e901f4
guipiccoli <guuipiccoli@gmail.com>
2018-11-22 15:05:19 -0200

    .

--- gerencia_memoria.py
+++ gerencia_memoria.py
@@ -1,3 +1,5 @@
+import sys, os
+
 class Node:
     def __init__(self, tipo, num):
         #livre/ocupado
@@ -53,33 +55,29 @@ class LinkedList:
                 print("Alocacao para o Bloco",nodo.num,"entre",nodo.start,"-",nodo.end)
                 return True
 
-        if aux.tipo == 'L' and (aux.end - aux.start) == tamanho:
-                aux.num = nodo.num
-                aux.tipo = 'S'
-                print("Alocacao para o Bloco",aux.num,"entre",aux.start,"-",aux.end)
-                return True
-
         #ultimo nodo da lista
         elif aux == self.tail:
             if not ((nodo,tamanho) in self.lista_espera):
                 self.lista_espera.append((nodo,tamanho))
-            self.fragmentacao(self.head, tamanho, 0)
             return False
         else:
             return self.add(aux.next, nodo, tamanho)
 
 
     #PRINTS / verifica se há fragmentação externa
-    def fragmentacao(self, nodo, tamanho, livre):
+    def fragmentacao(self, nodo, tamanho, espacoLivre):
         if nodo.tipo == "S":
-            print(nodo.start, "-", nodo.end, "bloco", nodo.num, "com tamanho igual a:", nodo.end - nodo.start)
+            print(nodo.start, "-", nodo.end, "bloco", nodo.num, " tamanho:", nodo.end - nodo.start)
         else:
-            livre = livre + (nodo.end - nodo.start)
-            print(nodo.start, "-", nodo.end, "livre, com tamanho igual a:", nodo.end - nodo.start)
+            espacoLivre = espacoLivre + (nodo.end - nodo.start)
+            print(nodo.start, "-", nodo.end, "livre, tamanho:", nodo.end - nodo.start)
         if nodo == self.tail:
-            print(livre," livres,", tamanho, "solicitados - fragmentacao externa")
+            if tamanho > espacoLivre:
+                print("Não há memória disponível", tamanho, "foi solicitado, e", espacoLivre, "esta livre")
+            else:
+                print(espacoLivre," livres,", tamanho, "solicitados - fragmentacao externa")
         else:
-            self.fragmentacao(nodo.next, tamanho, livre)
+            self.fragmentacao(nodo.next, tamanho, espacoLivre)
 
 
     #libera os blocos após reorganizar
@@ -91,7 +89,7 @@ class LinkedList:
             end = nodo.end
             print("Bloco",num, "(", start, "-", end, ") liberado")
             self.reorganiza(nodo)
-            self.espaco_livre()
+            self.espacoLivre()
         elif nodo == self.tail:
             print("Tamanho inválido.")
         else:
@@ -101,7 +99,7 @@ class LinkedList:
     #reorganiza os blocos para liberar memória
     def reorganiza(self, nodo):
         if nodo != self.head:
-            while nodo.prev.tipo == 'L':
+            while nodo.prev.tipo != 'S':
                 aux = nodo.prev
                 nodo.start = aux.start
                 nodo.prev = aux.prev
@@ -111,7 +109,7 @@ class LinkedList:
                 aux.prev.next = nodo
 
         if nodo != self.tail:
-            while nodo.next.tipo == 'L':
+            while nodo.next.tipo != 'S':
                 aux = nodo.next
                 nodo.end = aux.end
                 nodo.next = aux.next
@@ -122,7 +120,7 @@ class LinkedList:
 
 
     #Procura por espaço na lista para blocos na lista de espera
-    def espaco_livre(self):
+    def espacoLivre(self):
         bool = False
         for i in range(0,len(self.lista_espera)):
             nodo, tamanho = self.lista_espera[i]
@@ -130,11 +128,35 @@ class LinkedList:
             if bool:
                 #se conseguir adicionar na lista, deleta o mesmo da lista de espera
                 del self.lista_espera[i]
-                self.espaco_livre()
+                self.espacoLivre()
                 break
 
+    def estado_memoria(self, nodo, espacoLivre):
+        if nodo.tipo == "S":
+            print(nodo.start, "-", nodo.end, "bloco", nodo.num, "tamanho:", nodo.end - nodo.start)
+        else:
+            espacoLivre = espacoLivre + (nodo.end - nodo.start)
+            print(nodo.start, "-", nodo.end, "livre, tamanho:", nodo.end - nodo.start)
+
+        if nodo == self.tail:
+            quit()
+        else:
+            self.estado_memoria(nodo.next,espacoLivre)
+
 def main ():
-    file =  open('caso1.txt', 'r')
+    if len(sys.argv) == 1:
+        print("ERRO! Parâmetro não especificado")
+        print("Para rodar, utilize o seguinte comando, passando o caso de teste desejado sem a extensão .txt:")
+        print("$ python gerencia_memoria.py CASO_TESTE")
+        exit()
+
+    caso = sys.argv[1]
+    if not os.path.exists((caso +'.txt')):
+        print("Arquivo nao encontrado!!")
+        exit()
+
+    file = open(caso +'.txt', 'r')
+
     linha = file.readline()
     modoUtilizado = int(linha)
     linha = file.readline()
@@ -153,6 +175,10 @@ def main ():
             qtdBlocos += 1
         elif tipo == "L":
             listaNodos.liberaBloco(listaNodos.head,num)
+
+    print("\n Estado final da memória:")
+    listaNodos.estado_memoria(listaNodos.head,0)
+
     file.close()
 
 if __name__ == "__main__": main()
